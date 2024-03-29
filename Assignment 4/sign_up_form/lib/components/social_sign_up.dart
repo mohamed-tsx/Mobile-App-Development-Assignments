074e1971d601c9e7151d0d75e9b0e21e9fc638d4
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-29 17:26:50 +0300

    Added social registaration and footer

---
+++ Assignment 4/sign_up_form/lib/components/social_sign_up.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SocialSignUp extends StatelessWidget {
+  final Function()? onTap;
+  final ImagePath;
+  final String Social;
+  const SocialSignUp(
+      {super.key,
+      required this.onTap,
+      required this.ImagePath,
+      required this.Social});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(
+            color: Colors.grey, // Border color
+            width: 1, // Border width
+          ),
+        ),
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                ImagePath,
+                width: 25,
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Text(
+                "Sign In With $Social",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
--- Assignment 4/sign_up_form/lib/pages/registration_page.dart
+++ Assignment 4/sign_up_form/lib/pages/registration_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sign_up_form/components/sign_up_button.dart';
+import 'package:sign_up_form/components/social_sign_up.dart';
 import 'package:sign_up_form/components/textfield.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -9,7 +10,7 @@ class SignUpPage extends StatelessWidget {
   final emailController = TextEditingController();
   final confirmPasswordConroller = TextEditingController();
 
-  void SignUserUp() => {print("Hello World")};
+  void signUserUp() => {print("Hello World")};
 
   @override
   Widget build(BuildContext context) {
@@ -81,16 +82,59 @@ class SignUpPage extends StatelessWidget {
 
             // Create account button
             SignInButton(
-              onTap: SignUserUp,
+              onTap: signUserUp,
             ),
 
+            const SizedBox(
+              height: 15,
+            ),
             // Or Register using social media platforms
+            const Text(
+              "Or Register using social platforms",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
 
+            const SizedBox(
+              height: 15,
+            ),
             // Google
+            SocialSignUp(
+              onTap: signUserUp,
+              ImagePath: "Images/google.png",
+              Social: "Google",
+            ),
 
+            const SizedBox(
+              height: 15,
+            ),
             // Facebook
+            SocialSignUp(
+              onTap: signUserUp,
+              ImagePath: "Images/apple.png",
+              Social: "Apple",
+            ),
+            const SizedBox(
+              height: 60,
+            ),
 
             // Already have an account? Login
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already have an account?',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(width: 4),
+                const Text(
+                  'Logins',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       )),
