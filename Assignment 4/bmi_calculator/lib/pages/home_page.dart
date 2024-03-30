f0fa530905dbc61d59e54d86c8ea2d6d2f0d6f04
Mohamed <mohammedmohamuud.01@gmail.com>
2024-03-30 11:50:09 +0300

    Built the calculation button component

--- Assignment 4/bmi_calculator/lib/pages/home_page.dart
+++ Assignment 4/bmi_calculator/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/components/calculate_button.dart';
 import 'package:bmi_calculator/components/textFeild.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -7,6 +8,8 @@ class HomeScreen extends StatelessWidget {
   final weightController = TextEditingController();
   final heightController = TextEditingController();
   @override
+  void CalculateBmi() => {print("Hello World")};
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -24,13 +27,30 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               // TextFeild for the Height
-              
+              SizedBox(
+                height: 130,
+              ),
               BmiTextFeild(
                 controller: weightController,
                 hintText: "Enter Weight In Kg",
               ),
+
+              SizedBox(
+                height: 20,
+              ),
               // TextFeild for the Weight
+              BmiTextFeild(
+                controller: heightController,
+                hintText: "Enter Height In Cm",
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
               // Calculate button
+              CalculateButton(
+                onTap: CalculateBmi,
+              )
               // SHOW CASE THE BMI
             ],
           ),
